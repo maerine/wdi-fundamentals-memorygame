a14fe8ee7d5210ef5dcca0fcdd1116ae8d363b04
Mae Rine <mae.s.rine@gmail.com>
2019-08-14 22:08:08 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if(cardsInPlay.length === 2){
+function checkForMatch() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
   } else {
-    alert("Sorry try again");
+    console.log("Sorry try again");
   }
 }
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+  if(cardsInPlay.length === 2){
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
